d57290e164f5e096e17807ed3a3ff29233d86999
Igor Tabacki <igor@Igors-MacBook-Air.local>
2017-11-23 23:18:12 +0100

    YeahYeahYeah

---
+++ Swag/Swag/Controller/CategoryVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swag
+//
+//  Created by Igor Tabacki on 11/23/17.
+//  Copyright © 2017 Igor Tabacki. All rights reserved.
+//
+
+import UIKit
+
+class CategoryVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
